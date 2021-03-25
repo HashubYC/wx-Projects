7e740e4919d8a7c3aba74e79004bbc1941b22fb3
yc <912355998@qq.com>
2021-03-25 17:52:52 +0800

    promisic

--- sleeve-applet/utils/http.js
+++ sleeve-applet/utils/http.js
@@ -1,3 +1,5 @@
+import {promisic} from "./util";
+
 const {config} = require("../config/config");
 
 class Http {
@@ -16,6 +18,18 @@ class Http {
         })
     }
 }
+
+// wx.request
+// wx.request不要+()
+// 把一个函数当作参数传递到另一个函数
+// 动态类型的语言非常常见  java C# 需要委托
+promisic(wx.request)({ // 原来的wx.request传递什么参数，这个便传递什么参数
+    url: `${config.apiBaseUrl}${url}`,
+    data: data,
+})
+
+
+
 
 export {
     Http
